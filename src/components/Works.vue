3642cc07e88ff619735c12681b1d93a24e50cb12
Jasper Agrante <jasperneil0701@gmail.com>
2023-02-27 05:03:42 +0800

    WIP: working on planets

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -16,6 +16,7 @@
             outline
             rounded
             class="q-px-xl"
+            @click="visible = true"
           />
           <q-btn
             label="See my skills"
@@ -30,4 +31,98 @@
       </div>
     </div>
   </div>
+
+  <q-dialog
+    v-model="visible"
+    persistent
+    maximized
+  >
+    <q-card class="bg-black flex flex-center">
+      <div class="absolute-top-right">
+        <q-btn
+          v-close-popup
+          color="white"
+          label="Close"
+          flat
+        />
+      </div>
+      <div>
+        <div
+          v-for="galaxy, idx in galaxies"
+          :key="idx"
+        >
+          <div class="col-auto galaxy-name text-primary text-weight-bolder q-mt-lg q-mb-md">
+            Galaxy: {{ galaxy.name }}
+          </div>
+          <div class="row justify-center q-col-gutter-md">
+            <div
+              v-for="planet, planetIdx in galaxy.planets"
+              :key="`${idx}-${planetIdx}`"
+              class="col-auto"
+            >
+              <q-responsive
+                :ratio="1"
+                class="planet cursor-pointer non-selectable"
+              >
+                <div class="relative-position">
+                  <div class="absolute-center text-bold">
+                    {{ planet.name }}
+                  </div>
+                </div>
+              </q-responsive>
+            </div>
+          </div>
+        </div>
+      </div>
+    </q-card>
+  </q-dialog>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  emits: ['next'],
+  setup() {
+    const visible = ref(true)
+
+    const galaxies = [
+      {
+        name: 'Prism Code Philippines Inc.',
+        planets: [
+          { name: 'EvA' },
+          { name: 'Virtual Event' },
+          { name: 'Photobooth' },
+          { name: 'Registration' },
+          { name: 'Metaverse' },
+          { name: 'PrismNet' }
+        ]
+      },
+      {
+        name: 'Imergex Information Technologies, Inc.',
+        planets: [
+          { name: 'Travel Safe' },
+          { name: 'Swap Tayo' },
+          { name: 'Pan!c Button' }
+        ]
+      }
+    ]
+
+    return { visible, galaxies }
+  }
+})
+</script>
+
+<style lang="sass" scoped>
+.galaxy-name
+  font-size: 24px
+  text-align: center
+
+.planet
+  font-size: 32px
+  text-align: center
+  color: $primary
+  border: 1px solid white
+  border-radius: 50%
+  width: 200px
+</style>
